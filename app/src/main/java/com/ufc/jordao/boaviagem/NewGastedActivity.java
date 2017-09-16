5a8be602b1dfeeb64485c7e90cf964f269097ca6
pereiraneto <pereira.neto.12@gmail.com>
2017-09-16 02:27:16 -0300

    acrescentei dois metodos na viagem repository; fiz o layout da activity de novo gasto

--- app/src/main/java/com/ufc/jordao/boaviagem/NewGastedActivity.java
+++ app/src/main/java/com/ufc/jordao/boaviagem/NewGastedActivity.java
@@ -1,6 +1,8 @@
 package com.ufc.jordao.boaviagem;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -14,11 +16,16 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-public class NewGastedActivity extends Activity {
+import com.ufc.jordao.boaviagem.repository.ViagemRepository;
+
+public class NewGastedActivity extends Activity implements View.OnClickListener {
 
     private Spinner categoria;
+    private Spinner local;
+    private Button dataGasto;
+
+    static final int DATE_DIALOG_ID = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,5 +38,55 @@ public class NewGastedActivity extends Activity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categoria = (Spinner) findViewById(R.id.categoria);
         categoria.setAdapter(adapter);
+
+       /*
+        ViagemRepository viagemRepository = null;
+        ArrayList<String> destinos = viagemRepository.getAllDestinos();
+        ArrayAdapter adapter2 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, destinos);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        local = (Spinner) findViewById(R.id.local);
+        local.setAdapter(adapter2);*/
+
+        dataGasto = (Button) findViewById(R.id.dataGasto);
+        dataGasto.setOnClickListener(this);
+    }
+
+    public void onClickBreadcrumb(View v){
+        switch (v.getId()){
+            case R.id.toHomeBreadcrumb:
+                Intent newGastedActivity = new Intent(NewGastedActivity.this, HomeActivity.class);
+                startActivity(newGastedActivity);
+                break;
+        }
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        Calendar calendario = Calendar.getInstance();
+
+        int ano = calendario.get(Calendar.YEAR);
+        int mes = calendario.get(Calendar.MONTH);
+        int dia = calendario.get(Calendar.DAY_OF_MONTH);
+
+        switch (id) {
+            case R.id.dataGasto:
+                return new DatePickerDialog(this, dataGastoListener, ano, mes, dia);
+
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener dataGastoListener = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            Date date = new Date(year - 1900, monthOfYear, dayOfMonth);
+            SimpleDateFormat sd1 = new SimpleDateFormat("dd/MMM/yyyy");
+            dataGasto.setText(sd1.format(date));
+        }
+    };
+
+    @Override
+    public void onClick(View view) {
+        if (view == dataGasto)
+            showDialog(R.id.dataGasto);
     }
 }
